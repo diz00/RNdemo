95b65f48abf7ded5d6a3bc6ab20596ce1295d75d
diz00 <dizdiz0000@gmail.com>
2017-10-29 16:25:35 +0200

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, Text, View, Alert, AppRegistry } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './src/store';
+import AuthScreen from './src/screens/AuthScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 export default class jobs extends Component {
+
   render() {
+      const MainNavigator = TabNavigator({
+        welcome: { screen: WelcomeScreen },
+        auth: { screen: AuthScreen },
+        main: {
+          screen: TabNavigator({
+            map: { screen: MapScreen },
+            deck: { screen: DeckScreen },
+            review: {
+              screen: StackNavigator({
+                review: { screen: ReviewScreen },
+                settings: { screen: SettingsScreen }
+              })
+            }
+          }, { 
+            tabBarPosition: 'bottom', 
+            swipeEnabled: false, 
+            tabBarOptions: {
+              labelStyle: { fontSize: 12 },
+              showIcon: true,
+            } 
+          })
+        }
+      },
+      {
+        lazy: true,
+        navigationOptions: {
+          tabBarVisible: false 
+        }
+      });
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('jobs', () => jobs);
